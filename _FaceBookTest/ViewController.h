9f152285f1dee6e9ecfcc9f7707e1e7fd5e8a928
MrReN <renhaoit@163.com>
2015-01-06 20:23:45 +0800

    Initial Commit

---
+++ _FaceBookTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  _FaceBookTest
+//
+//  Created by 任浩 on 15/1/6.
+//  Copyright (c) 2015年 任浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ _FaceBookTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  _FaceBookTest
+//
+//  Created by 任浩 on 15/1/6.
+//  Copyright (c) 2015年 任浩. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
